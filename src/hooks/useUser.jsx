11b3e18b38e6330fcb5d8e6f06758f6c9cef4a7f
nur-nayeem <nurnayem768@gmail.com>
2025-12-13 02:54:16 +0600

    added custom hook useUser

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxios from "./useAxios";
+
+const useUser = () => {
+  const { user } = useAuth();
+  const axiosInstance = useAxios();
+
+  const {
+    isLoading: userLoading,
+    data: currentUser = null,
+    refetch: refetchUser,
+  } = useQuery({
+    queryKey: ["current-user", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosInstance.get(`/users/${user.email}`);
+      return res.data.user || null;
+    },
+  });
+
+  return { currentUser, userLoading, refetchUser };
+};
+
+export default useUser;
