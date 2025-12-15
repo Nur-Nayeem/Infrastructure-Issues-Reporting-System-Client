ef0197e49afb9acf05f0cacf115512e62d30fb71
nur-nayeem <nurnayem768@gmail.com>
2025-12-15 12:18:54 +0600

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { use } from "react";
 import { MdDashboard, MdLogout } from "react-icons/md";
 import { Link, NavLink } from "react-router";
 import { AuthContext } from "../../context/Contexts";
+import { Logo } from "../Logo/Logo";
 
 const Navbar = ({ open, setOpen }) => {
   const navLinks = [
@@ -87,29 +88,3 @@ const Navbar = ({ open, setOpen }) => {
   );
 };
 export default Navbar;
-
-export const Logo = () => {
-  return (
-    <Link to="/" className="flex items-center gap-3 text-white">
-      <div className="size-8 text-primary">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M12 21v-2a4 4 0 0 0-4-4H4a4 4 0 0 0-4 4v2"></path>
-          <path d="M16 17v-2a4 4 0 0 0-4-4H4"></path>
-          <path d="M20 17v-2a4 4 0 0 0-4-4h-4"></path>
-          <circle cx="12" cy="5" r="4"></circle>
-        </svg>
-      </div>
-      <h2 className="text-white text-xl font-display font-semibold tracking-wide">
-        CivicResolve
-      </h2>
-    </Link>
-  );
-};
